05911d4c76850184843a435e9bda22009b91981b
effiekaras <72948462+effiekaras@users.noreply.github.com>
2022-12-04 17:23:23 -0600

    Implement disney movies

--- src/DetailComponent/Detail.js
+++ src/DetailComponent/Detail.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import moment from 'moment'
 
 import axios from 'axios';
 import './Detail.scss';
@@ -16,15 +17,33 @@ import {
     MDBModalFooter,
   } from 'mdb-react-ui-kit';
 
+  const getImage = (path) => `https://image.tmdb.org/t/p/w342/${path}`;
+  const api_key = "59dd51057d034c78c09b0129b62b2de9";
+  const BASE_URL = "https://api.themoviedb.org/3";
+
+
 
 function Detail() {
     const {id} = useParams();
-    const [data, setData] = React.useState("");
-    const [type, setType] = React.useState("");
-    const [description, setDescription] = React.useState("")
     const [basicModal, setBasicModal] = useState(false);
     const [lists, setLists] = React.useState([]);
     const toggleShow = () => setBasicModal(!basicModal);
+    const api = axios.create({ baseURL: BASE_URL });
+
+    const [data, setData] = React.useState([]);
+    const [movieTitle, setMovieTitle] = React.useState([]);
+    const [overview, setOverview] = React.useState([]);
+    const [image, setImage] = React.useState("");
+    const [genre, setGenre] = React.useState([]);
+    const [release, setRelease] = React.useState("");
+
+    function getImageFunc(path) {
+        if (path) {
+          return getImage(path);
+        } else {
+          return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtrPMpWbHOGEXiyZ7au75btz1wRmc60SL7aobwQWRUj7rGIw3a1VSISsx1y1xuiVOPG54&usqp=CAU`;
+        }
+    }
 
     // Used to initialize lists for testing; replace with real lists
     React.useEffect(() => {
@@ -37,123 +56,25 @@ function Detail() {
 
         setLists([list1,list2, list3, list4, list5])
     }, [])
-
+    
 
 
     React.useEffect(() => {
-        const film = fetch(`https://ghibliapi.herokuapp.com/films/${id}`)
-        .then((response) => response.json())
-        .then((d) => {
-            if (d.name || d.title) {
-                setType("film");
-                setData(d);
-            }
-            else {
-                const ppl = fetch(`https://ghibliapi.herokuapp.com/people/${id}`)
-                .then((response) => response.json())
-                .then((d) => {
-                if (d.name || d.title) {setData(d)
-                    setType("people")}
-                else {
-                    const loc = fetch(`https://ghibliapi.herokuapp.com/locations/${id}`)
-                .then((response) => response.json())
-                .then((d) => {
-                if (d.name || d.title) {setData(d);
-                setType("location")}
-                else {
-                    const veh = fetch(`https://ghibliapi.herokuapp.com/vehicles/${id}`)
-                .then((response) => response.json())
-                .then((d) => {
-                if (d.name || d.title) {setData(d);
-                setType("vehicle");}
-            }); 
-                }
-            }); 
-                }
-            }); 
-            }
-        }) 
-    }, []);
-
-    React.useEffect(() => {
-        if (type === "film") {
-            setDescription(`${data.description || "NA"}
-            Release Date: ${data.release_date || "NA"}
-            Director: ${data.director || "NA"}
-            Producer: ${data.producer || "NA"}
-            Running Time: ${data.running_time || "NA"} min.
-            Other Titles: ${data.original_title || "NA"}, ${data.original_title_romanised|| "NA"}`);
-        } else if (type === "people") {
-            (async () => {
-                let film = data.films[0]
-                var film_title = "NA"
-                let species_url = data.species
-                var species = "NA"
-                await fetch(film)
-                    .then((response) => response.json())
-                    .then((d) => {film_title = d.title
-                });
-    
-                await fetch(species_url)
-                .then((response) => response.json())
-                .then((d) => {species = `${d.name}, ${d.classification}`
-                });
-    
-                setDescription(`
-                Gender: ${data.gender || "NA"}
-                Age: ${data.age|| "NA"}
-                Eye Color: ${data.eye_color|| "NA"}
-                Hair Color: ${data.hair_color|| "NA"}
-                Film: ${film_title || "NA"}
-                Species: ${species || "NA"}
-                `)
-                })();
-        } else if (type === "location") {
-            (async () => {
-            let film = data.films[0]
-            var film_title = "NA"
-            await fetch(film)
-                .then((response) => response.json())
-                .then((d) => {film_title = d.title
-            });
-            var climate = data.climate;
-            if (climate == "TODO") {
-                climate = "NA"
-            }
-            var terrain = data.terrain;
-            if (terrain == "TODO") { terrain = "NA"}
-            setDescription(`
-            Climate: ${climate || "NA"}
-            Terrain: ${terrain || "NA"}
-            Film: ${film_title || "NA"}
-            `)
-        })();
-        } else if (type == "vehicle") {
-            (async () => {
-            let film = data.films[0]
-            var film_title = "NA"
-            var pilot = "NA"
-            await fetch(film)
-                .then((response) => response.json())
-                .then((d) => {film_title = d.title
-            });
-            await fetch(data.pilot)
-                .then((response) => response.json())
-                .then((d) => {pilot = d.name
-            });
-            setDescription(`
-            ${data.description || "NA"}
-            Type: ${data.vehicle_class || "NA"}
-            Length: ${data.length || "NA"} ft.
-            Pilot: ${pilot || "NA"}
-            Film: ${film_title || "NA"}
-            `)
-        })();
-        } else {
-            setDescription("");
-        }
-    })
-
+        var link = `movie/${id}?api_key=${api_key}&language=en-US`;
+        const movie = api.get(link);
+        movie.then((res) => {
+        var mov = res.data;
+        setData(mov);
+        setMovieTitle(mov.original_title);
+        setOverview(mov.overview);
+        setRelease(mov.release_date);
+        var genres = []
+        mov.genres.forEach((element) => {
+        genres.push(element.name);
+        });
+        setGenre(genres);
+        setImage(getImageFunc(mov.poster_path));
+    })}, [])
 
     const favorite = event => {
         // Add logic for favoriting here
@@ -171,24 +92,25 @@ function Detail() {
     }
 
 
-    return (
-        
+return (
     <section> 
     <div className="grid" id="detail">
     <div className="container">
     <div className="modal-content">
-    <h1>{data.title || data.name} <i className="fa-regular fa-heart" onClick={(e)=>{favorite(e)}}></i></h1>
-    <img className="detailimg" src={data.image  || require(`../images/${(data.title || data.name || "grey").replaceAll(' ', '_').replaceAll('\'', '')}.png`)}/>
+    <h1> {data.original_title} <i className="fa-regular fa-heart" onClick={(e)=>{favorite(e)}}></i></h1>
+    <img className="gridimg" src={getImageFunc(data.poster_path)}/>
     <p className="description">
-       {description} 
+            {overview || "No overview available."} <br></br>
+            Release Date: {moment(release, 'Y/M/D').format('MMMM D, Y')} <br></br>
+            Genre: {JSON.stringify(genre, null, 2).replace(/['"]+/g, '').replace(/['[]+/g, '').replace(/['\]]+/g, '') || "N/A"} 
     </p>
     <div className="detailButtons">
     <>
-      <Button className="detailButton" onClick={toggleShow}>
+        <Button className="detailButton" onClick={toggleShow}>
         Add to List
-      </Button>
+        </Button>
 
-      <Modal className="modal" show={basicModal}  tabIndex='-1'>
+        <Modal className="modal" show={basicModal}  tabIndex='-1'>
         <button id="x" onClick={toggleShow}>&#10006;</button>
         <div className="modalBox">
             <span id="modalTitle">Which list would you like to add this to?</span>
@@ -201,7 +123,7 @@ function Detail() {
             </div>
             <button onClick={CreateNewList} id="createList">Create a New List</button>
         </div>
-      </Modal>
+        </Modal>
     </>
 
     <button className=" detailButton">
@@ -213,5 +135,4 @@ function Detail() {
     </section>
     )
 }
-
 export default Detail;
--- src/GalleryComponent/Gallery.js
+++ src/GalleryComponent/Gallery.js
@@ -4,69 +4,33 @@ import axios from 'axios';
 import './Gallery.scss';
 import { getDefaultNormalizer, waitFor } from '@testing-library/react';
 
-const BASE_URL = 'https://ghibliapi.herokuapp.com'
-
-
+const BASE_URL = "https://api.themoviedb.org/3";
+const getImage = (path) => `https://image.tmdb.org/t/p/w342/${path}`;
+const api_key = "59dd51057d034c78c09b0129b62b2de9";
 
 function Gallery() {
-    const [films, setFilms] = React.useState([]);
-    const [people, setPeople] = React.useState([]);
-    const [locations, setLocations] = React.useState([]);
-    const [vehicles, setVehicles] = React.useState([]);
+  const api = axios.create({ baseURL: BASE_URL });
+
+    const [movies, setMovies] = React.useState([]);
     const [button, setButton] = React.useState("");
     const [filteredData, setFilteredData] = React.useState([]);
     const [data, setData] = React.useState([]);
+    const [buttonData, setButtonData] = React.useState([]);
     const [searchInput, setSearchInput] = React.useState("");
+    const [genre, setGenre] = React.useState("");
     
     const handleClick = (e) => {
         e.preventDefault();
         setButton(e.target.value);
       }; 
 
-    const getFilms = () =>  {
-        const data = fetch("https://ghibliapi.herokuapp.com/films")
-        .then((response) => response.json())
-        .then((data) => {
-        setFilms(data)})
-        .catch(rejected => {
-            console.log(rejected);
-        });
-      };
-
-    const getPeople = () => {
-        const data = fetch("https://ghibliapi.herokuapp.com/people")
-        .then((response) => response.json())
-        .then((data) => {
-        setPeople(data)
+      function getImageFunc(path) {
+        if (path) {
+          return getImage(path);
+        } else {
+          return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtrPMpWbHOGEXiyZ7au75btz1wRmc60SL7aobwQWRUj7rGIw3a1VSISsx1y1xuiVOPG54&usqp=CAU`;
         }
-        )
-        .catch(rejected => {
-            console.log(rejected);
-        });
-    };
-
-    const getLocations = async () => {
-        const data = fetch("https://ghibliapi.herokuapp.com/locations")
-        .then((response) => response.json())
-        .then((data) => {
-        setLocations(data)
-        }
-        )
-        .catch(rejected => {
-            console.log(rejected);
-        });
-    };
-
-    const getVehicles = async () => {
-        const data = fetch("https://ghibliapi.herokuapp.com/vehicles")
-        .then((response) => response.json())
-        .then((data) => {
-        setVehicles(data)}
-        )
-        .catch(rejected => {
-            console.log(rejected);
-        });
-    };
+      }
 
     const handleChange = (e) => {
         setSearchInput(e.target.value.toLowerCase());
@@ -74,101 +38,178 @@ function Gallery() {
 
    
     React.useEffect(() => {
-      getFilms();
-      getPeople();
-      getLocations();
-      getVehicles();
+      setData([]);
+      setMovies([]);
+      setFilteredData([]);
+      setButtonData([]);
+      setGenre("");
+      var movs = []
+      for (let i = 1; i < 34; i++) {
+      var link =`/discover/movie?api_key=${api_key}&language=en-US&page=${i}&with_companies=2`
+      const mov = api.get(link);
+      mov.then((res) => {
+        if (i == 1) {
+          setMovies(res.data.results)
+        } else {
+        setMovies((curr) => {
+          return [
+            ...curr,
+            ...res.data.results,
+          ];
+        }); }
+    });
+  }
   }, []); 
 
-    React.useEffect(() => {
-      setData(() => {
-        return [
-          ...films,
-          ...people,
-          ...locations,
-          ...vehicles
-        ]; 
-      })
-      setFilteredData(() => {
-        return [
-          ...films,
-          ...people,
-          ...locations,
-          ...vehicles
-        ]; 
-      });
-    }, [vehicles])
- 
-    
+  
 
     React.useEffect(() => {
+      setGenre("");
+      setData(movies)
+      setFilteredData(movies);
+      setButtonData(movies);
+    }, [movies])
+  
+    React.useEffect(() => {
         if (searchInput.length > 0) {
-          setFilteredData(() => {
-            return [
-              ...films,
-              ...people,
-              ...locations,
-              ...vehicles
-            ]; 
-          });
-          const searchedData = data.filter(
+          const searchedData = buttonData.filter(
             d => {
               return (
-                (d.name || d.title).toLowerCase().includes(searchInput.toLowerCase())
+                (d.original_title).toLowerCase().includes(searchInput.toLowerCase())
                )
             }
           )
           setFilteredData(searchedData);
         } else {
-        if (button === 'films') {
-            setFilteredData(films);
-        } else if (button === 'characters') {
-            setFilteredData(people);
-        } else if (button === 'settings') {
-            setFilteredData(locations);
-        } else if (button === 'vehicles') {
-            setFilteredData(vehicles);
-        } else {
-            setFilteredData(() => {
-                return [
-                  ...films,
-                  ...people,
-                  ...locations,
-                  ...vehicles
-                ];
-              });
+        if (button === '28') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(28)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("action ")
+        } else if (button == '12') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(12)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("adventure ")
+        } else if (button == '80') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(80)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("crime ")
+        }
+        else if (button == '35') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(35)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("comedy ")
+        }  else if (button == '18') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(18)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("drama ")
+        }  else if (button == '14') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(14)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("fantasy ");
+        }  else if (button == '9648') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(9648)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("mystery ");
+        }  else if (button == '10749') {
+          const filter = data.filter(
+            d=> {
+              return (
+                (d.genre_ids).includes(10749)
+              )
+            }
+          )
+          setFilteredData(filter);
+          setButtonData(filter);
+          setGenre("romance ");
+        }
+        else {
+            setFilteredData(movies);
+            setButtonData(movies);
+            setGenre("")
         } }
       }, [searchInput, button]);
-
     return (
         <header id="gallery-body">
     <div className="searchBox">
       <i className="fa-solid fa-magnifying-glass mag"></i>
         <input className="searchbar"
           type="search"
-          placeholder="search for..."
+          placeholder={"search for " + genre + "movies..."}
           onChange={handleChange}
           value={searchInput} />
     </div>
 
-        <section className="buttonBar">
+    <section className="buttonBar">
           <button className="button" value="" onClick={e => handleClick(e)}>All</button>
-          <button className="button" value="films" onClick={e => handleClick(e)}>Films</button>
-          <button className="button" value="characters" onClick={e => handleClick(e)}>Characters</button>
-          <button className="button" value="settings" onClick={e => handleClick(e)}>Settings</button>
-          <button className="button" value="vehicles" onClick={e => handleClick(e)}>Vehicles</button>
+          <button className="button" value="28" onClick={e => handleClick(e)}>Action</button>
+          <button className="button" value="12" onClick={e => handleClick(e)}>Adventure</button>
+          <button className="button" value="80" onClick={e => handleClick(e)}>Crime</button>
+          <button className="button"value="35" onClick={e => handleClick(e)}>Comedy</button>
+          <button className="button" value="18" onClick={e => handleClick(e)}>Drama</button>
+          <button className="button" value="14" onClick={e => handleClick(e)}>Fantasy</button>
+          <button className="button" value="9648" onClick={e => handleClick(e)}>Mystery</button>
+          <button className="button" value="10749" onClick={e => handleClick(e)} >Romance</button>
           </section>
 
         <div className="grid">
-        {filteredData.map((f) => (
-          <Link className="linkWrapper" to={`/detail/${f.id}`}>
+        {filteredData.map((movie) => (
+        <Link className="linkWrapper" to={`/detail/${movie.id}`}>
           <div className="item">
-            <img className="gridimg" src={f.image || require(`../images/${(f.title || f.name || "grey").replaceAll(' ', '_').replaceAll('\'', '')}.png`)}/>
+          <img className="gridimg" src={getImageFunc(movie.poster_path)}/>
             <p className="narrow">
-            {f.title || f.name}</p> 
+            {movie.original_title || "N/A"}</p>
           </div>
           </Link>
-        ))}         
+        ))}  
       </div>
     </header>
     )
