9c7440b6abaeb6cb9e022a0b11cddd5e45df6ea1
angeldong0826 <adong6@illinois.edu>
2022-12-10 16:35:04 -0600

    finished implementing register

--- client/src/AuthComponents/Login.js
+++ client/src/AuthComponents/Login.js
@@ -2,11 +2,63 @@ import React, {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import axios from 'axios';
 import './Login.scss';
+import { Navigate, useNavigate  } from 'react-router-dom';
 
-function Login() {
+function Login(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [currentForm, setCurrentForm] = useState('login');
+    let user = [];
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+
+        fetch('http://localhost:4000/api/users', {
+            body: {"email": email, "password": password}
+            
+        }).then((data) => {
+            user = data.clone().json();
+            console.log(data.clone().json());
+            console.log(data.status);
+            // return data.json();
+        })
+
+        if (currentForm === 'login') {
+            navigate('/register');
+        }
+    }
+
+    
+
+    console.log(user);
+    console.log("hi");
+
+
     return (
-        <div>
-            <h1>login component</h1>
+        <div className="auth-form-container">
+            <h1>Login:</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="yourEmail@gmail.com" id="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="******" id="password" name="password" />
+                </div>
+                <div>
+                    <button type="submit">Log In</button>
+                </div>
+                
+                {/* <button className="link" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button> */}
+                
+            </form>
+            <button className="link" onClick={navigate('/register')}>Don't have an account? Register here.</button>
+            
+            
         </div>
     )
 }
--- client/src/AuthComponents/Register.js
+++ client/src/AuthComponents/Register.js
@@ -2,11 +2,107 @@ import React, {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import axios from 'axios';
 import './Register.scss';
+import { Navigate, useNavigate  } from 'react-router-dom';
 
-function Register() {
+
+function Register(props) {
+    const [em, setEmail] = useState('');
+    const [pass, setPassword] = useState('');
+    const [currentForm, setCurrentForm] = useState('register');
+    const [user, setUsername] = useState('');
+    const [na, setName] = useState('');
+
+    const navigate = useNavigate();
+
+    const asyncPostCall = async () => {
+        try {
+            const response = await fetch('http://localhost:4000/api/users', {
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({
+         // your expected POST request payload goes here
+                 username: user,
+                 email: em,
+                 name: na,
+                 password: pass
+                })
+             });
+             const data = await response.json();
+             console.log(data);
+
+             if (data.status === 500) {
+                console.log("user already exists");
+             }
+
+           } catch(error) {
+               
+              console.log(error)
+             } 
+        }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(email);
+        console.log(user);
+        console.log(typeof user);
+
+        // fetch('http://localhost:4000/api/users', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         "username": user, 
+        //         "name": na, 
+        //         "email": em, 
+        //         "password": pass})
+        // })
+        // .then((data) => {
+        //     console.log(data.status)
+        //     console.log(data.data)
+
+        //     if (data.status === 500) {
+        //         console.log("user already exists")
+        //     }
+        // })
+        
+        asyncPostCall();
+
+        navigate('/login');
+    }
+
+    const nav = (e) => {
+        e.preventDefault();
+        navigate('/login');
+    }
+
     return (
-        <div>
-            <h1>register component</h1>
+        <div className="auth-form-container">
+            <div>
+                <h1>Sign Up:</h1>
+                <form className="register-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Username:</label>
+                        <input value={user} onChange={(e) => setUsername(e.target.value)} name="username" id="username" placeholder="username"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="name">Full Name:</label>
+                        <input value={na} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="name"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input value={em} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" /> 
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input value={pass} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******" id="password" name="password" /> 
+                    </div>
+
+                    <button type="submit">Log In</button>
+                    
+                </form>
+                {/* <button className="link" onClick={() => props.onFormSwitch('login')}>Already have an account? Log in here.</button> */}
+                <button className="link" onClick={nav}>Already have an account? Login here.</button>
+            </div>
         </div>
     )
 }
